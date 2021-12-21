35de36e31ad9aaf5b7727f5e9223587b6de40f33
Daniel Rosendorf <daniel.rosendorf@googlemail.com>
2021-12-21 14:13:46 +0100

    Add ESLint, Prettier and pre-commit hook

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-undef */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    semi: ['error', 'always'],
+    'prettier/prettier': 'error',
+  },
+};
